a665831ab2486374d4428187fd01645ae05c20fb
ChristineWeidhofer <xtine@gmx.at>
2023-02-16 14:17:26 +0100

    advanced exercise start

--- Christine/script.js
+++ Christine/script.js
@@ -1,7 +1,7 @@
 "use strict";
-// let vitamin: string = "B";
-// console.log(vitamin);
+// Basic 1
 let selector = document.getElementById("result");
+let selWrap = document.getElementById("wrapper");
 class Person {
     constructor(name, age, jobTitle) {
         this.name = name;
@@ -9,9 +9,80 @@ class Person {
         this.jobTitle = jobTitle;
     }
     printGreet() {
-        return `<p>Hello there, my name is ${this.name}, I am ${this.age} years old and I am a ${this.jobTitle}.</p>`;
+        return `Hello there, my name is ${this.name}, I am ${this.age} years old and I am a ${this.jobTitle}`;
     }
 }
 const person1 = new Person("Christine", 45, "Web Developer");
 console.log(person1);
 selector.innerHTML = person1.printGreet();
+// Basic 2
+class PersonSalary extends Person {
+    constructor(name, age, jobTitle, salary, jobLocation) {
+        super(name, age, jobTitle);
+        this.salary = salary;
+        this.jobLocation = jobLocation;
+    }
+    printSalary() {
+        return `<hr><br>${super.printGreet()}, I get ${this.salary} every month and I work in ${this.jobLocation}.`;
+    }
+}
+const person2 = new PersonSalary("Christine", 45, "Web Developer", 3000, "San Francisco");
+console.log(person2);
+selector.innerHTML += person2.printSalary();
+// Advanced
+class Vehicles {
+    constructor(brand, year, color, price) {
+        this.brand = brand;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+    }
+    printAd() {
+        return `<hr><br>Hello there, our car is made by ${this.brand}, it is from ${this.year} and is ${this.color}`;
+    }
+    printPrice() {
+        return `€ ${this.price},--`;
+    }
+}
+const vehicle1 = new Vehicles("Mercedes", 1999, "silver", 50000);
+const vehicle2 = new Vehicles("Audi", 2015, "blue", 5000);
+const vehicle3 = new Vehicles("BMW", 2022, "red", 30000);
+const vehicle4 = new Vehicles("VW", 2019, "black", 10000);
+console.log(vehicle1);
+console.log(vehicle2);
+console.log(vehicle3);
+console.log(vehicle4);
+// selector.innerHTML += vehicle1.printAd();
+// selector.innerHTML += vehicle1.printPrice();
+let allVehicles = [vehicle1, vehicle2, vehicle3, vehicle4]; //create array of objects
+console.log(allVehicles);
+for (let val of allVehicles) {
+    selWrap.innerHTML += ` 
+  <div class="box">${val.brand}</div>
+  `;
+}
+let boxs = document.getElementsByClassName("box");
+for (let i = 0; i < boxs.length; i++) {
+    boxs[i].addEventListener("click", function () {
+        boxs[i].style.backgroundColor = "white";
+        boxs[i].innerHTML = `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${allVehicles[i].brand}</h5>
+      <p class="card-text">This vehicle was built in ${allVehicles[i].year}</p>
+      <a href="#" class="btn btn-primary btnPrice">Price</a>
+    </div>
+    </div>
+    `;
+        let buttons = document.getElementsByClassName("btnPrice");
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener("click", function () {
+                //console.log(`The price of the vehicle is: € ${allVehicles[i].price}`);
+                alertPrice();
+            });
+        }
+    });
+    function alertPrice() {
+        alert(`The price of the vehicle is: € ${allVehicles[i].price}`);
+    }
+}
--- Christine/script.ts
+++ Christine/script.ts
@@ -1,7 +1,8 @@
-// let vitamin: string = "B";
-// console.log(vitamin);
+
+// Basic 1
 
 let selector = document.getElementById("result")as HTMLElement;
+let selWrap = document.getElementById("wrapper")as HTMLElement;
 
 class Person {
   name: string;
@@ -13,7 +14,7 @@ class Person {
       this.jobTitle = jobTitle;
     }
     printGreet() {
-      return `<p>Hello there, my name is ${this.name}, I am ${this.age} years old and I am a ${this.jobTitle}.</p>`;
+      return `Hello there, my name is ${this.name}, I am ${this.age} years old and I am a ${this.jobTitle}`;
     }  
 }
 
@@ -21,4 +22,92 @@ const person1 = new Person("Christine", 45, "Web Developer");
 
 console.log(person1);
 selector.innerHTML = person1.printGreet();
+
+// Basic 2
+
+class PersonSalary extends Person {
+  salary: number;
+  jobLocation: string;
+    constructor(name: string, age: number, jobTitle: string, salary: number, jobLocation: string) {
+      super(name, age, jobTitle)
+      this.salary = salary;
+      this.jobLocation = jobLocation;
+    }
+  printSalary() {
+    return `<hr><br>${super.printGreet()}, I get ${this.salary} every month and I work in ${this.jobLocation}.`;
+  }  
+}
+
+const person2 = new PersonSalary("Christine", 45, "Web Developer", 3000, "San Francisco");
+
+console.log(person2);
+selector.innerHTML += person2.printSalary();
+
+// Advanced
+
+class Vehicles {
+  brand: string;
+  year: number;
+  color: string;
+  price: number;
+    constructor(brand: string, year: number, color: string, price: number) {
+      this.brand = brand;
+      this.year = year;
+      this.color = color;
+      this.price = price;
+    }
+    printAd() {
+      return `<hr><br>Hello there, our car is made by ${this.brand}, it is from ${this.year} and is ${this.color}`;
+    }  
+    printPrice() {
+      return `€ ${this.price},--`;
+    }
+}
+const vehicle1 = new Vehicles("Mercedes", 1999, "silver", 50000);
+const vehicle2 = new Vehicles("Audi", 2015, "blue", 5000);
+const vehicle3 = new Vehicles("BMW", 2022, "red", 30000);
+const vehicle4 = new Vehicles("VW", 2019, "black", 10000);
+
+console.log(vehicle1);
+console.log(vehicle2);
+console.log(vehicle3);
+console.log(vehicle4);
+// selector.innerHTML += vehicle1.printAd();
+// selector.innerHTML += vehicle1.printPrice();
+
+let allVehicles = [vehicle1, vehicle2, vehicle3, vehicle4]; //create array of objects
+console.log(allVehicles);
+
+for (let val of allVehicles) {
+  selWrap.innerHTML += ` 
+  <div class="box">${val.brand}</div>
+  `;
+}
+
+let boxs = document.getElementsByClassName("box") as HTMLCollection;
+
+for(let i: number = 0; i < boxs.length; i++){
+  boxs[i].addEventListener("click", function(){
+    (boxs[i] as HTMLElement).style.backgroundColor = "white";
+    boxs[i].innerHTML = `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${allVehicles[i].brand}</h5>
+      <p class="card-text">This vehicle was built in ${allVehicles[i].year}</p>
+      <a href="#" class="btn btn-primary btnPrice">Price</a>
+    </div>
+    </div>
+    `;
+    let buttons = document.getElementsByClassName("btnPrice")as HTMLCollection;
+    for (let i: number = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+    //console.log(`The price of the vehicle is: € ${allVehicles[i].price}`);
+    alertPrice();
+    })
+    }
+  })
+  function alertPrice() {
+    alert(`The price of the vehicle is: € ${allVehicles[i].price}`);
+  }
+}
 
