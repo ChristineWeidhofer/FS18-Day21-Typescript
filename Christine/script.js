7370f96ff465f2da588c4070d54f680db4348733
ChristineWeidhofer <xtine@gmx.at>
2023-02-16 10:49:16 +0100

    basic 1

--- Christine/script.js
+++ Christine/script.js
@@ -1,3 +1,17 @@
 "use strict";
-let vitamin = "B";
-console.log(vitamin);
+// let vitamin: string = "B";
+// console.log(vitamin);
+let selector = document.getElementById("result");
+class Person {
+    constructor(name, age, jobTitle) {
+        this.name = name;
+        this.age = age;
+        this.jobTitle = jobTitle;
+    }
+    printGreet() {
+        return `<p>Hello there, my name is ${this.name}, I am ${this.age} years old and I am a ${this.jobTitle}.</p>`;
+    }
+}
+const person1 = new Person("Christine", 45, "Web Developer");
+console.log(person1);
+selector.innerHTML = person1.printGreet();
--- Christine/script.ts
+++ Christine/script.ts
@@ -1,2 +1,24 @@
-let vitamin: string = "B";
-console.log(vitamin);
+// let vitamin: string = "B";
+// console.log(vitamin);
+
+let selector = document.getElementById("result")as HTMLElement;
+
+class Person {
+  name: string;
+  age: number;
+  jobTitle: string;
+    constructor(name: string, age: number, jobTitle: string) {
+      this.name = name;
+      this.age = age;
+      this.jobTitle = jobTitle;
+    }
+    printGreet() {
+      return `<p>Hello there, my name is ${this.name}, I am ${this.age} years old and I am a ${this.jobTitle}.</p>`;
+    }  
+}
+
+const person1 = new Person("Christine", 45, "Web Developer");
+
+console.log(person1);
+selector.innerHTML = person1.printGreet();
+
